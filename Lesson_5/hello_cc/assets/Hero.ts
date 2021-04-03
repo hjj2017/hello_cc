e8477b696288b369cffcd6458d3b20aef7c2f36b
HAIJIANG JIN <hjj2021@outlook.com>
2021-04-03 23:38:21 +0800

    添加课程代码；

--- Lesson_5/hello_cc/assets/Hero.ts
+++ Lesson_5/hello_cc/assets/Hero.ts
@@ -1,4 +1,5 @@
 // @import
+import Async, { NextStep } from "./Async";
 import MyHeroFactory from "./MyHeroFactory";
 
 // @const
@@ -33,6 +34,116 @@ export default class Hero extends cc.Component {
      * onLoad
      */
     onLoad(): void {
+        let i: number;
+
+        // 第 0 个版本
+        ////////////////
+        i = 0;
+        Async.seriez_0([
+            () => {
+                i++;
+                console.log("这是第一个函数, i = " + i);
+
+                cc.resources.load("Test", (_, oAssetz) => {
+                    // 我们可以看到,
+                    // oAssetz = Hello 这句话还没有打印出来,
+                    // 第二个函数就被执行完了...
+                    cc.log("oAssetz = " + oAssetz);
+                });
+            },
+
+            () => {
+                i++;
+                console.log("这是第二个函数, i = " + i);
+            },
+
+            () => {
+                i++;
+                console.log("这是第三个函数, i = " + i);
+            }
+        ]);
+
+        // 第 1 个版本
+        ////////////////
+        let oNextStep = new NextStep();
+        i = 0;
+        Async.seriez_1(oNextStep, [
+            () => {
+                i++;
+                console.log("这是第一个函数, i = " + i);
+
+                cc.resources.load("Test", (_, oAssetz) => {
+                    // 我们可以看到,
+                    // oAssetz = Hello 这句话打印完成之后,
+                    // 第二个函数才被执行...
+                    cc.log("oAssetz = " + oAssetz);
+                    oNextStep.doNext();
+                });
+            },
+
+            () => {
+                i++;
+                console.log("这是第二个函数, i = " + i);
+                oNextStep.doNext();
+            },
+
+            () => {
+                i++;
+                console.log("这是第三个函数, i = " + i);
+            }
+        ]);
+
+        // 第 2 个版本
+        ////////////////
+        i = 0;
+        Async.seriez_2([
+            (oNextStep) => {
+                i++;
+                console.log("这是第一个函数, i = " + i);
+
+                cc.resources.load("Test", (_, oAssetz) => {
+                    cc.log("oAssetz = " + oAssetz);
+                    oNextStep.doNext();
+                });
+            },
+
+            (oNextStep) => {
+                i++;
+                console.log("这是第二个函数, i = " + i);
+                oNextStep.doNext();
+            },
+
+            (oNextStep) => {
+                i++;
+                console.log("这是第三个函数, i = " + i);
+            }
+        ]);
+
+        // 第 3 个版本
+        ////////////////
+        i = 0;
+        Async.seriez_3([
+            (funDoNext) => {
+                i++;
+                console.log("这是第一个函数, i = " + i);
+
+                cc.resources.load("Test", (_, oAssetz) => {
+                    cc.log("oAssetz = " + oAssetz);
+                    funDoNext();
+                });
+            },
+
+            (funDoNext) => {
+                i++;
+                console.log("这是第二个函数, i = " + i);
+                funDoNext();
+            },
+
+            (funDoNext) => {
+                i++;
+                console.log("这是第三个函数, i = " + i);
+            }
+        ]);
     }
 
     /**
@@ -46,7 +157,17 @@ export default class Hero extends cc.Component {
             }
 
             this._oHeroNode = oHeroNode;
-            cc.find("Canvas").addChild(this._oHeroNode);
+            cc.find("Canvas").addChild(oHeroNode);
+        });
+
+        MyHeroFactory.createAsync("Hero_Skeleton", (oHeroNode: cc.Node) => {
+            if (null == oHeroNode) {
+                cc.error("创建英雄失败!");
+                return;
+            }
+
+            //this._oHeroNode = oHeroNode;
+            cc.find("Canvas").addChild(oHeroNode);
         });
 
         this.node.on(cc.Node.EventType.TOUCH_END, (oEvent: cc.Event.EventTouch) => {
--- Lesson_5/hello_cc/assets/MyHeroFactory.ts
+++ Lesson_5/hello_cc/assets/MyHeroFactory.ts
@@ -1,3 +1,5 @@
+import Async from "./Async";
+
 const BUNDLE_NAME = "customBundlez";
 
 /**
@@ -22,74 +24,100 @@ export default class MyHeroFactory {
             return;
         }
 
-        // 获取已加载的 Bundle
-        let oLoadedBundle = cc.assetManager.getBundle(BUNDLE_NAME);
+        // 已加载的 Bundle
+        let oLoadedBundle = null;
+        let oLoadedPrefab = null;
 
-        // 如果 Bundle 没有被加载过
-        if (null == oLoadedBundle) {
-            // 尝试加载 Bundle
-            cc.assetManager.loadBundle(BUNDLE_NAME, (oError: Error, oBundle: cc.AssetManager.Bundle) => {
-                if (null != oError) {
-                    cc.error(oError);
+        Async.seriez_3([
+            // 先加载 Bundle
+            (funDoNext) => {
+                oLoadedBundle = cc.assetManager.getBundle(BUNDLE_NAME);
+
+                if (null != oLoadedBundle) {
+                    funDoNext();
                     return;
                 }
 
-                if (null == oBundle) {
-                    cc.error(`Bundle 为空! bundleName = ${BUNDLE_NAME}`);
+                // 尝试加载 Bundle
+                cc.assetManager.loadBundle(BUNDLE_NAME, (oError: Error, oBundle: cc.AssetManager.Bundle) => {
+                    if (null != oError) {
+                        cc.error(oError);
+                        return;
+                    }
+
+                    if (null == oBundle) {
+                        cc.error(`Bundle 为空! bundleName = ${BUNDLE_NAME}`);
+                        return;
+                    }
+
+                    oLoadedBundle = oBundle;
+                    funDoNext();
+                });
+            },
+
+            // 加载 Prefab
+            (funDoNext) => {
+                if (null == oLoadedBundle) {
                     return;
                 }
-
-                oLoadedBundle = oBundle;
 
                 // 获取已加载的预制体 Prefab
                 let strPrefabName = `hero/prefab/${strHeroName}`;
-                let oLoadedPrefab = oLoadedBundle.get(strPrefabName, cc.Prefab);
+                oLoadedPrefab = oLoadedBundle.get(strPrefabName, cc.Prefab);
 
                 // 如果预制体没有被加载过
+                if (null != oLoadedPrefab) {
+                    funDoNext();
+                    return;
+                }
+
+                // 尝试加载预制体
+                oLoadedBundle.load(strPrefabName, (oError: Error, oPrefab: cc.Prefab) => {
+                    if (null != oError) {
+                        cc.error(oError);
+                        return;
+                    }
+
+                    if (null == oPrefab) {
+                        cc.error(`预制体为空! prefabName = ${strPrefabName}`);
+                        return;
+                    }
+
+                    oLoadedPrefab = oPrefab;
+                    funDoNext();
+                });
+            },
+
+            // 预加载龙骨资源
+            (funDoNext) => {
+                // 
+                // 考虑到网络延迟问题,
+                // 在创建预制体实例之前先加载龙骨动画相关资源...
+                // 
+                // 因为 .prefab 是文本文件,
+                // 龙骨动画含有图片资源,
+                // 而加载资源都是通过 HTTP GET 方式加载的,
+                // 所以就可能出现 "英雄节点" 创建成功且已经加到画布上了,
+                // 但是图片资源没加载完, 动画显示不正常!
+                oLoadedBundle.preloadDir(`hero/dragonBonez/${strHeroName}`, (oError: Error) => {
+                    if (null != oError) {
+                        cc.error(oError);
+                        return;
+                    }
+
+                    funDoNext();
+                });
+            },
+
+            // 创建预制体实例
+            (funDoNext) => {
                 if (null == oLoadedPrefab) {
-                    // 尝试加载预制体
-                    oLoadedBundle.load(strPrefabName, (oError: Error, oPrefab: cc.Prefab) => {
-                        if (null != oError) {
-                            cc.error(oError);
-                            return;
-                        }
-
-                        if (null == oPrefab) {
-                            cc.error(`预制体为空! prefabName = ${strPrefabName}`);
-                            return;
-                        }
-
-                        // 
-                        // 考虑到网络延迟问题,
-                        // 在创建预制体实例之前先加载龙骨动画相关资源...
-                        // 
-                        // 因为 .prefab 是文本文件,
-                        // 龙骨动画含有图片资源,
-                        // 而加载资源都是通过 HTTP GET 方式加载的,
-                        // 所以就可能出现 "英雄节点" 创建成功且已经加到画布上了,
-                        // 但是图片资源没加载完, 动画显示不正常!
-                        oLoadedBundle.preloadDir(`hero/dragonBonez/${strHeroName}`, (oError: Error) => {
-                            if (null != oError) {
-                                cc.error(oError);
-                                return;
-                            }
-
-                            let oHeroNode = cc.instantiate(oPrefab);
-                            funCallback(oHeroNode);
-                        });
-                    });
+                    return;
                 }
-                else {
-                    // 
-                    // 我们还需要处理 oLoadedBundle 不为空的情况,
-                    // 可是拜 TypeScript 的异步回调机制所赐, 回调函数嵌套太深了...
-                    // 重构逻辑会特别复杂!
-                    // 
-                }
-            });
-        }
-        else {
-            
-        }
+
+                let oHeroNode = cc.instantiate(oLoadedPrefab);
+                funCallback(oHeroNode);
+            }
+        ]);
     }
 }
